488b1cd992aca9b89913345c317b12b55777b489
Maxim Orlov <mail@orlovmax.com>
2014-09-29 23:21:18 +0300

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,237 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+
+      //Assemble *.js files
+        concat: {
+          all: {
+              src: ['dev/js/*.js', '!dev/js/assembled.js'],
+              dest: 'dev/js/assembled.js'
+          }
+        },
+
+      //Uglify assembled *.js file    
+        uglify: {
+            options: {
+              mangle: false
+            },
+            vendor: {
+              files: [{
+                  expand: true,
+                  cwd: 'dev/js/vendor',
+                  src: '*.js',
+                  dest: 'www/js/vendor',
+                  ext: '.min.js'
+              }]
+            },
+            main: {
+                files: {
+                    'www/js/assembled.min.js': '<%= concat.all.dest %>'
+                }
+            }
+        },
+
+      //Compile *.scss files  
+        sass: {
+          all: { 
+            options: {
+              style: 'expanded',
+              sourcemap: 'none'
+            },
+            files: [{
+              expand: true,
+              cwd: 'dev/styles',
+              src: ['*.{sass, scss}'],
+              dest: 'dev/css',
+              ext: '.css'
+            }]
+          }
+         },
+
+      //Compile *.less files
+        less: {
+          files: {
+             expand: true,
+             cwd: 'dev/styles',
+             src: ['*.less'],
+             dest: 'dev/css',
+             ext: '.css'
+          }          
+        },  
+
+      //Combine media queries in result *.css files   
+        cmq: {
+          options: {
+            log: false
+          },
+          your_target: {
+            files: {
+              'dev/css': ['dev/css/*.css']
+            }
+          }
+        },
+
+      //Autoprefixer  
+        autoprefixer: {
+          options: {
+            browsers: ['last 2 versions', 'ie 8', 'ie 9'] 
+            //By default >1%, last 2 versions, Firefox ESR, Opera 12.1;
+          },           
+          files: {
+            expand: true,
+            flatten: true,
+            src: 'dev/css/*.css',
+            dest: 'dev/css/'
+          }
+        },
+
+      //Minify and organize *.css files  
+        csso: {
+          files: {
+            expand: true,
+            cwd: 'dev/css/',
+            src: ['*.css', '!*.min.css'],
+            dest: 'www/css/',
+            ext: '.min.css'
+          }
+        },
+
+      //Compile *.jade files  
+        jade: {
+          compile: {
+            options: {
+                client: false,
+                pretty: true
+            },
+            files: [ {
+              cwd: "dev/markup",
+              src: "*.jade",
+              dest: "dev/",
+              expand: true,
+              ext: ".html"
+            } ]
+          }
+        },
+
+      //Compile *.haml files  
+        haml: {
+          compile: {
+            files: [ {
+              cwd: "dev/markup",
+              src: "*.haml",
+              dest: "dev/",
+              expand: true,
+              ext: ".html"
+            } ]
+          }
+        },
+
+      //Minify *.html files 
+        htmlmin: {  
+            all: {     
+              options: {             
+                collapseWhitespace: true,
+                minifyJS: true,
+                minifyCSS: true
+              },
+              files: [ {
+                cwd: "dev/",
+                src: "*.html",
+                dest: "www/",
+                expand: true,
+                ext: ".html"
+              } ]
+            }
+        },
+
+      //Minify image files   
+        imagemin: {
+          all: { 
+            options: { 
+              optimizationLevel: 7
+            },              
+            files: [{
+              expand: true,      
+              cwd: 'dev/img',            
+              src: ['*.{png,jpg,gif}'], 
+              dest: 'www/img/'            
+            }]
+          }
+        },
+
+      //Assemble bower components in right order 
+        bower_concat: {
+          all: {
+            dest: 'dev/js/vendor/vendor.js',
+            exclude: 'jquery'
+          }
+        },
+
+      //Copy bower components to the custom folder   
+        bowercopy: {
+          options: {
+            clean: false
+          },          
+          jquery: {
+            options: {
+                destPrefix: 'dev/js/vendor'
+            },
+            files: {
+              
+            },
+          }
+        },
+
+      //Watch for changes    
+        watch: {
+          all: {
+            files: ['dev/*.html', 
+                    'dev/styles/*.scss', 
+                    'dev/css/*.css', 
+                    'dev/js/*.js',  
+                    'dev/js/**/*.js', 
+                    'dev/img/*.{png,jpg,gif}',
+                    'dev/markup/*.{haml,jade}' ],
+            tasks: ['default'],
+            options: {
+              spawn: false,
+            },
+          },
+        }
+
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    // grunt.loadNpmTasks('grunt-combine-media-queries');
+    // grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-csso');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-haml');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-newer');
+
+    grunt.loadNpmTasks('grunt-bowercopy');
+    grunt.loadNpmTasks('grunt-bower-concat');
+
+    grunt.registerTask('default', ['newer:concat', 
+                                   'newer:uglify', 
+                                   'newer:sass', 
+                                   'newer:less',
+                                   // 'newer:cmq',
+                                   // 'newer:autoprefixer', 
+                                   'newer:csso',
+                                   'newer:jade',
+                                   'newer:haml',
+                                   'newer:htmlmin',
+                                   'newer:imagemin',
+                                   'watch'
+    ]);
+
+    grunt.registerTask('bower-dev', ['bowercopy',
+                                     'bower_concat'
+    ]);
+};
---
+++ README.md
@@ -0,0 +1,98 @@
+# Front-end scaffold
+
+My front-end boilerplate based on [grunt-boilerplate](https://github.com/orlovmax/grunt-boilerplate), have the same folder structure but have predefined dependencies for Bower and boileplate-files for styles, markup and javascript
+
+## Folder and file structure
+
+```
+.
+├── Gruntfile.js
+├── package.json
+├── bower.json
+├── README.md
+├── dev
+│   ├── index.html // compiled markup html file
+│   ├── css
+│   │   └── screen.css // compiled css with autoprefixer processing
+│   ├── img
+│   │   └── *.png, *.jpg, *.gif //image sources
+│   ├── markup
+│   │   └── index.haml, index.jade // marrkup templates sources
+│   ├── js
+│   │   ├── custom.js // custom scripts and plugins sources
+│   │   └── vendor
+│   │       ├── vendor.js // concatenated vendor scripts
+│   │       └── jquery.js // libs sources
+│   ├── styles
+│   │   └── screen.sass, screen.scss, screen.less // preprocessor styles
+│   └── fonts //font sources
+│
+└── www
+    ├── index.html // minified html file
+    ├── css
+    │   └── screen.min.css // minified styles
+    ├── img
+    │   └── *.png, *.jpg, *.gif // minified images
+    ├── js
+    │   ├── assembled.js // assembled custom scripts and plugins
+    │   ├── assembled.min.js // minified assembled custom scripts and plugins
+    │   └── vendor
+    │       ├── vendor.min.js // concatenated and minified vendor scripts
+    │       └── jquery.min.js // minified vendor libs
+    └── fonts // @font-face-ready webfonts
+
+```        
+Source files  placed in `dev` folder, where you should provide your development process.
+
+## How to start
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to use [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins.
+
+* Before start you need to have _npm_ installed now, as well as a _grunt_.
+* Then you need to download this files. You can chose `Download zip` option or clone this repo to your local maschine.
+* Now go to this project folder in terminal. Once you're familiar with grunt installation process, you may install all this things with this command: `npm install`.This will create `node_moduldes` folder, that's normal.
+* Run `grunt`, and it will watch for all the changes.
+
+##Package.json dependencies 
+* ["grunt"](https://github.com/gruntjs)
+* ["grunt-autoprefixer"](https://github.com/nDmitry/grunt-autoprefixer)
+* ["grunt-bower-concat"](https://github.com/sapegin/grunt-bower-concat)
+* ["grunt-bowercopy"](https://github.com/timmywil/grunt-bowercopy)
+* ["grunt-combine-media-queries"](https://github.com/buildingblocks/grunt-combine-media-queries)
+* ["grunt-contrib-concat"](https://github.com/gruntjs/grunt-contrib-concat)
+* ["grunt-contrib-haml"](https://github.com/jhchen/grunt-contrib-haml)
+* ["grunt-contrib-htmlmin"](https://github.com/gruntjs/grunt-contrib-htmlmin)
+* ["grunt-contrib-imagemin"](https://github.com/gruntjs/grunt-contrib-imagemin)
+* ["grunt-contrib-jade"](https://github.com/gruntjs/grunt-contrib-jade)
+* ["grunt-contrib-less"](https://github.com/gruntjs/grunt-contrib-less)
+* ["grunt-contrib-sass"](https://github.com/gruntjs/grunt-contrib-sass)
+* ["grunt-contrib-uglify"](https://github.com/gruntjs/grunt-contrib-uglify)
+* ["grunt-contrib-watch"](https://github.com/gruntjs/grunt-contrib-watch)
+* ["grunt-csso"](https://github.com/t32k/grunt-csso)
+* ["grunt-newer"](https://github.com/tschaub/grunt-newer)
+
+##Bower
+From the version 1.2.0 this boilerplate ready for work with [Bower](https://github.com/bower/bower). So at the root was created `bower.json` file with project dependencies. To install dependencies you need globally instaled Bower. 
+Bower depends on [Node.js](http://nodejs.org/) and [npm](http://npmjs.org/). Also make sure that [git](http://git-scm.com/) is installed as some bower
+packages require it to be fetched and installed. To install Bower - just run this `$ npm install -g bower`.
+Now we are ready to install components:
+```sh
+# install dependencies listed in bower.json
+$ bower install
+
+# install a package and add it to bower.json
+$ bower install <package> --save
+```
+
+Now we can run our grunt task, by default called bower-dev `grunt bower-dev`.
+This task run two other tasks: `bower-concat` will concatenate specified components in right order and `bowercopy` will copy main or selected files of components from bower_components folder to specified folders in you project.
+Please note, that this tasks need to be specified according to packages that were installed in your project.   
+More details about this tasks you can read here:
+* ["grunt-bower-concat"](https://github.com/sapegin/grunt-bower-concat)
+* ["grunt-bowercopy"](https://github.com/timmywil/grunt-bowercopy)
+
+## Changelog
+* v1.0.0 (September 29, 2014)
+    * Initial commit
+
+## License
+[MIT](http://opensource.org/licenses/MIT)
