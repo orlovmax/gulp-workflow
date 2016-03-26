596efdd99a072ad3354d2c608cac787d321b538a
Maxim Orlov <mail@orlovmax.com>
2016-03-26 19:16:52 +0200

    common used js added

--- dev/js/body.js
+++ dev/js/body.js
@@ -1,1 +1,7 @@
+// Include body scripts in the right order
+// They will be concatenated using gulp-rigger in body.min.js
+
 //= vendor/jquery.js
+//= lib/jquery.img-fit.js
+//= lib/jquery.sticky-header.js
+//= lib/jquery.toggle-nav.js
