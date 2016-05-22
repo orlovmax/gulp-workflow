20010c1ef9d421c8df999f3b3ae7e4f763e0685e
Maxim Orlov <mail@orlovmax.com>
2016-05-22 04:56:14 +0300

    minor fix

--- _automation/_gulp/gulp_tasks/postcss.js
+++ _automation/_gulp/gulp_tasks/postcss.js
@@ -4,7 +4,6 @@ var gulp = require('gulp'),
 	plumber = require('gulp-plumber'),
 	postcss = require('gulp-postcss'),
 	autoprefixer = require('autoprefixer'),
-	cmq = require('gulp-merge-media-queries'),
 	csso = require('postcss-csso');
 
 
