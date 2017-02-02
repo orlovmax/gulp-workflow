7aaa7c013c2b7a1020b141a2ac065d475fa869e1
Maxim Orlov <mail@orlovmax.com>
2017-02-02 03:57:05 +0200

    ie.js process removed from grunt rigger task

--- _automation/_grunt/grunt_tasks/rigger.js
+++ _automation/_grunt/grunt_tasks/rigger.js
@@ -2,7 +2,6 @@
 module.exports = {
 	main: {
 		files: [{
-			'<%= paths.build.js %>/ie.min.js': '<%= paths.dev.js %>/ie.js',
 			'<%= paths.build.js %>/body.min.js': '<%= paths.dev.js %>/body.js',
 			'<%= paths.build.js %>/head.min.js': '<%= paths.dev.js %>/head.js'
 		}]
